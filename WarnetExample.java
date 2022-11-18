19cf7aba0981abbd21c32751f25f058679c84b45
rafiansyah23 <114588092+rafiansyah23@users.noreply.github.com>
2022-11-18 13:22:39 +0700

    Add files via upload

---
+++ WarnetExample.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utsa_18012022;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class WarnetExample {
+     public static void main(String[] args) {
+        WarnetRecord pelanggan = new WarnetRecord();
+
+        pelanggan.setKode(1);
+        pelanggan.setNamaPelanggan("rafiansyah");
+        pelanggan.setJenisPelanggan(1);
+        pelanggan.setTglMasuk(2);
+        pelanggan.setJamMasuk(5);
+        pelanggan.setJamKeluar(8);
+        pelanggan.setLama(2);
+
+        pelanggan.Print();
+        pelanggan.Pembayaran();
+
+
+
+
+    }
+}
--- WarnetRecord.java
+++ WarnetRecord.java
@@ -10,22 +10,166 @@ package utsa_18012022;
  * @author LENOVO
  */
 public class WarnetRecord {
-     public static void main(String[] args) {
-        WarnetExamle pelanggan = new WarnetExamle();
-
-        pelanggan.setKode(1);
-        pelanggan.setNamaPelanggan("rafiansyah");
-        pelanggan.setJenisPelanggan(1);
-        pelanggan.setTglMasuk(2);
-        pelanggan.setJamMasuk(5);
-        pelanggan.setJamKeluar(8);
-        pelanggan.setLama(2);
-
-        pelanggan.Print();
-        pelanggan.Pembayaran();
+     private int kode;
+    private String NamaPelanggan;
+    private int JenisPelanggan;
+    private int TglMasuk;
+    private int JamMasuk;
+    private int JamKeluar;
+    private double Tarif;
+    private double Total;
+    private int lama;
+    private int harga;
+    private double potongan;
 
 
-
-
+    public void PembayaranPengobatan (double potongan, double Total, int lama, int harga, int kode,String NamaPelanggan, int JenisPelanggan, int TglMasuk, int JamMasuk,int JamKeluar, double Tarif ){
+       this.kode = kode; 
+       this.JenisPelanggan = JenisPelanggan;
+       this.TglMasuk = TglMasuk;
+       this.JamMasuk = JamMasuk;
+       this.JamKeluar = JamKeluar;
+       this.Tarif = Tarif;
+       this.Total = Total;
+       this.lama = lama;
+       this.harga = harga;
+       this.potongan = potongan;
     }
+
+    ////////////////////////////
+     public int getKode() {
+        return kode;
+    }
+    public int getJenisPelanggan() {
+        return JenisPelanggan;
+    }
+    public int getTglMasuk() {
+        return TglMasuk;
+    }
+
+    public int getJamMasuk() {
+        return JamMasuk;
+    }
+
+    public double getTotal() {
+        return Total;
+    }
+
+    public int getLama() {
+        return lama;
+    }
+
+
+
+    public int getJamKeluar() {
+        return JamKeluar;
+    }
+    public double getTarif() {
+        return Tarif;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public double getPotongan() {
+        return potongan;
+    }
+    ///////////////////////////////
+    public void setKode(int kode) {
+        this.kode = kode;
+    }
+
+    public void setNamaPelanggan(String namaPelanggan) {
+        this.NamaPelanggan = namaPelanggan;
+    }
+
+    public void setJenisPelanggan(int jenisPelanggan) {
+        this.JenisPelanggan = jenisPelanggan;
+    }
+
+    public void setTglMasuk(int tglMasuk) {
+        this.TglMasuk = tglMasuk;
+    }
+
+    public void setJamMasuk(int jamMasuk) {
+        this.JamMasuk = jamMasuk;
+    }
+
+    public void setJamKeluar(int jamKeluar) {
+        this.JamKeluar = jamKeluar;
+    }
+
+    public void setTarif(int tarif) {
+        this.Tarif = tarif;
+    }
+
+    public void setTotal(double total) {
+        this.Total = total;
+    }
+
+    public void setLama(int lama) {
+        this.lama = lama;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+    public void setPotongan(int potongan) {
+        this.potongan = potongan;
+    }
+
+    ////////////////////////////
+    public int Pembayaran(){
+        switch (JenisPelanggan) {
+            case 1 :
+                harga = 4000;
+                lama = JamKeluar - JamMasuk;
+                if(lama>2){
+                 potongan =  (harga * 0.02);
+                 Tarif = Tarif * potongan;
+                 Total = lama * Tarif - potongan;
+                }else{
+                 potongan = (harga * 0);
+                 Tarif = Tarif * potongan;
+                 Total = lama * Tarif - potongan;
+                } 
+                break;
+             case 2 :
+                harga = 6000;
+                lama = JamKeluar - JamMasuk;
+                if(lama>2){
+                 potongan = (int) (harga * 0.05);
+                 Tarif = Tarif * potongan;
+                 Total = lama * Tarif - potongan;
+                }else{
+                 potongan = (int) (harga * 0);
+                 Tarif = Tarif * potongan;
+                 Total = lama * Tarif - potongan;
+                } 
+                break;
+                default:
+            break;
+        }
+        System.out.println("Harga    = " + harga);
+        System.out.println("Potongan = " + potongan);
+        System.out.println("Total Pembayaran = " + Total);
+        return (int) Total;
+    }
+    public void Print(){
+        System.out.println("============== Warnet ==============");
+        System.out.println("Jenis Pelanggan");
+        System.out.println("1. VIP     = 4000");
+        System.out.println("2. GOLD    = 6000");
+        System.out.println("---------------------------------------------------");
+        System.out.println("Diskon");
+        System.out.println("1. VIP = potongan 2% jika leboh dari 2 jam");
+        System.out.println("2. Gold = potongan 5% jika lebih dari 2 jam");
+        System.out.println("---------------------------------------------------");
+        System.out.println("Kode = " + kode);
+        System.out.println("Nama Pelanggan = " + NamaPelanggan);
+        System.out.println("Lama  = " + lama);
+        System.out.println("---------------------------------------------------");
+
+    } 
 }
